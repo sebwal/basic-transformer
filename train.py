66a4580bf8eb25e3afe35bf068d8280d80c9fa40
sebwal <seb.walter93@gmail.com>
2019-05-31 11:52:10 +0800

    adapt train.py to update weights more or less arbitrarily (just for testing)

--- train.py
+++ train.py
@@ -9,7 +9,17 @@ EPOCHS = 200
 
 def main():
     transformer = Transformer()
-    sample = transformer()
+    optimizer = torch.optim.Adam(transformer.parameters(), lr=0.0002)
+    loss = torch.nn.BCELoss()
+
+    for _ in range(1000):
+        sample = transformer()
+        target = torch.ones(sample.shape[0], sample.shape[1])
+        error = loss(sample, target)
+        if _ % 10 == 0: print(error)
+        error.backward()
+        optimizer.step()
+        if(error > 8): break
     print(sample)
 
 if __name__ == "__main__":
